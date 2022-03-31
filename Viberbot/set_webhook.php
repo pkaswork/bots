64479aaff17e9b46f3e81890c47d2d2ba0e71e7f
pkaswork <58027173+pkaswork@users.noreply.github.com>
2022-03-31 12:09:13 +0300

    Рефакторинг

--- Viberbot/set_webhook.php
+++ Viberbot/set_webhook.php
@@ -1,21 +1,33 @@
 <?php
-	$auth_token = 'AUTH_TOKEN';
-	$webhook = 'WEBHOOK_URL';
+$auth_token = 'AUTH_TOKEN';
+$webhook = 'WEBHOOK_URL';
 	
-	$jsonData = 
-	'{
-		"auth_token": "'.$auth_token.'",
-		"url": "'.$webhook.'",
-		"event_types": ["subscribed", "unsubscribed", "delivered", "message", "seen"]
-	}';
+$jsonData = 
+'{
+	"auth_token": "'.$auth_token.'",  // Вроде тут не нужен token https://developers.viber.com/docs/api/rest-bot-api/#post-data
+	"url": "'.$webhook.'",
+	"event_types": 	[
+	      "delivered",
+	      "seen",
+	      "failed",
+	      "subscribed",
+	      "unsubscribed",
+	      "conversation_started"
+	],
+	"send_name": true,
+	"send_photo": true
+}';
 	
-	$ch = curl_init('https://chatapi.viber.com/pa/set_webhook');
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-	$response = curl_exec($ch);
-	$err = curl_error($ch);
-	curl_close($ch);
-	if($err) {echo($err);}
-	else {echo($response);}
+$ch = curl_init('https://chatapi.viber.com/pa/set_webhook');
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+$response = curl_exec($ch);
+$err = curl_error($ch);
+curl_close($ch);
+if($err) {
+	echo($err);
+} else {
+	echo($response);
+}
 ?>
